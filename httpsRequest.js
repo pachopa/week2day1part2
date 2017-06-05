ca732a553c3dc3471cdb72521821aaca74257cad
chris <pachopa0320@gmail.com>
2017-06-05 20:51:14 +0000

    making http request using the request HTTP client

---
+++ httpsRequest.js
@@ -0,0 +1,12 @@
+// require `request` and the Node `fs` (filesystem) module
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples')               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err; 
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+       })
+       .pipe(fs.createWriteStream('./downloaded.html'));    
